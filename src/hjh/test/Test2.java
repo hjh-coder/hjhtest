7b33db51c54b7258e4ba2c9cf5700fbe0b1f8536
hjh <hjh@DESKTOP-ED0TKRB>
2025-01-02 13:11:39 +0900

    Merge branch 'hjh-branch' of https://github.com/hjh-coder/hjhtest.git into hjh-branch

--- src/hjh/test/Test2.java
+++ src/hjh/test/Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
 	
 	public void test() {
 		System.out.println("test - branch");
+		
+		System.out.println("test2 - branch");
 	}
 }
--- src/hjh/test2/Test3.java
+++ src/hjh/test2/Test3.java
@@ -1,5 +1,7 @@
 package hjh.test2;
 
 public class Test3 {
-	
+	public void Test() {
+		System.out.println("test333 - branch");
+	}
 }
