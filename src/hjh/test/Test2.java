1690f89a13295ac7a6da2a0076123ab0af489adb
hjh <hjh@DESKTOP-ED0TKRB>
2025-01-06 15:00:42 +0900

    Merge branch 'master' of https://github.com/hjh-coder/hjhtest.git

--- src/hjh/test/Test2.java
+++ src/hjh/test/Test2.java
@@ -14,11 +14,15 @@ public class Test2 {
 		System.out.println("test5 - web edit");
 
 		System.out.println("test6 - web edit");
+		
+		System.out.println("test7 - web edit");
 	}
 	
 	public void test2() {
 		System.out.println("test !!!!!!!!!! ");
 		
-		System.out.println("test 222222222222 !!!!!!!!!! ");
+		System.out.println("test 333333333333 !!!!!!!!!! ");
+		
+		System.out.println("test 444444444444 !!!!!!!!!! ");
 	}
 }
