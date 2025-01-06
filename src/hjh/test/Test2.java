8af6e1216bc7c7232d3fcc68fb634916be8f9dbf
hjh <hjh@DESKTOP-ED0TKRB>
2025-01-06 15:19:03 +0900

    commit

--- src/hjh/test/Test2.java
+++ src/hjh/test/Test2.java
@@ -27,6 +27,12 @@ public class Test2 {
 		
 		System.out.println("test 666666666666 !!!!!!!!!! ");
 		
+		System.out.println("test 888888888888 !!!!!!!!!! ");
+		
 		System.out.println("test 999999999999 !!!!!!!!!! ");
+		
+		System.out.println("test 000000000000 !!!!!!!!!! ");
+		
+		System.out.println("test 111111111111 !!!!!!!!!! ");
 	}
 }
