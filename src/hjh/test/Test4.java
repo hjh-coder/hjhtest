f3c12d7d1444f794a533c0a1f21493d3803b727c
hjh-coder <hjh@jsoldev.com>
2025-01-06 15:40:45 +0900

    Update Test4.java
    test4 web commit

--- src/hjh/test/Test4.java
+++ src/hjh/test/Test4.java
@@ -4,11 +4,5 @@ public class Test4 {
 	
 	public void test() {
 		System.out.println("test4 - branch");
-		
-		System.out.println("test4 - branch");
-		
-		System.out.println("test4 - branch");
-		
-		System.out.println("test4 - branch");
 	}
 }
